e5ef28397721bec86974925cb80ee69640517d74
Skyreach <Crystal@skyreachstudios.com>
2020-10-17 11:38:59 -0400

    commit activity 3

---
+++ Activity1Part3/Activity1Part3/Activity1Part3/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Activity1Part3.Models;
+using Activity1Part3.Services.Business;
+
+namespace Activity1Part3.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserModel model) {
+            // Validate the Form POST 
+            if (!ModelState.IsValid)
+                return View("Login");
+
+
+            SecurityService service = new SecurityService();
+
+
+            bool status = service.Authenticate(model);
+
+            if (status)
+            {
+                return View("LoginPassed",model);
+            }
+            else
+            {
+                return View("LoginFailed");
+            }
+
+        }
+
+
+    }
+}
---
+++ Activity1Part3/Activity1Part3/Activity1Part3/Services/Data/SecurityDAO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Activity1Part3.Models;
+using System.Data.SqlClient;
+
+namespace Activity1Part3.Services.Data
+{
+    public class SecurityDAO
+    {
+        public bool FindByUser(UserModel user) {
+            string conncectionString = "Data Source=DESKTOP-BEPLE8D;Initial Catalog=Test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            string query = @"SELECT * FROM dbo.Users WHERE dbo.Users.USERNAME = " + "'" + user.Username + "'";
+            bool results = false;
+            using ( SqlConnection connection = new SqlConnection(conncectionString))
+            {
+                try
+                {
+
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            string foo = reader.GetString(1);
+                            if (reader.GetString(1).Equals(user.Username))
+                            {
+                                results = true;
+                            }
+                        }
+                    }
+
+                 
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+            return results;
+        }
+    }
+}
